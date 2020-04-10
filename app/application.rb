343c1ecf7cf2ea4833ff7e2dd3c000d50b7eec1f
Steven Perfors <stevecanbuild@gmail.com>
2020-04-10 15:01:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,4 +5,13 @@ class Application
     
     time = Time.now.hour 
     
+    if time <= 12
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end
     
+    resp.finish 
+  end
+  
+end
